35106fdb318c71a7969ad1bbcd4f31ee59d23997
shreelathasuranagi <shreelathasuranagi@gmail.com>
2024-10-11 21:34:44 +0530

    Classes and Objects

---
+++ bookDetails.java
@@ -0,0 +1,180 @@
+public class bookDetails {
+    String title;
+    String author;
+    String genre;
+    int pages;
+    int price;
+
+    public static void main(String[] args){
+
+        // Create an object of the class 'bookDetails'
+        bookDetails book = new bookDetails();
+        book.title = "The Seven Husbands of Evelyn Hugo";
+        book.author = "Taylor Jenkins Reid";
+        book.genre = "Historical Fiction";
+        book.pages = 400;
+        book.price = 999;
+
+        // Print book details
+        System.out.println("Book Title: " + book.title);
+        System.out.println("Author: " + book.author);
+        System.out.println("Genre: " + book.genre);
+        System.out.println("Pages: " + book.pages);
+        System.out.println("Price: Rs. " + book.price);
+
+        System.out.println("--------------------------------------------------------------");
+        
+        //book1
+        bookDetails book1 = new bookDetails();
+        book1.title = "The Midnight Library";
+        book1.author = "Matt Haig";
+        book1.genre = "Fantasy";
+        book1.pages = 304;
+        book1.price = 850;
+
+        // Print book1 details
+        System.out.println("Book Title: " + book1.title);
+        System.out.println("Author: " + book1.author);
+        System.out.println("Genre: " + book1.genre);
+        System.out.println("Pages: " + book1.pages);
+        System.out.println("Price: Rs. " + book1.price);
+
+        System.out.println("--------------------------------------------------------------");
+
+        //book2
+        bookDetails book2 = new bookDetails();
+        book2.title = "Atomic Habits";
+        book2.author = "James Clear";
+        book2.genre = "Self-help";
+        book2.pages = 320;
+        book2.price = 599;
+
+        // Print book2 details
+        System.out.println("Book Title: " + book2.title);
+        System.out.println("Author: " + book2.author);
+        System.out.println("Genre: " + book2.genre);
+        System.out.println("Pages: " + book2.pages);
+        System.out.println("Price: Rs. " + book2.price);
+
+        System.out.println("--------------------------------------------------------------");
+
+        //book3
+        bookDetails book3 = new bookDetails();
+        book3.title = "Sapiens: A Brief History of Humankind";
+        book3.author = "Yuval Noah Harari";
+        book3.genre = "History";
+        book3.pages = 498;
+        book3.price = 999;
+
+        // Print book3 details
+        System.out.println("Book Title: " + book3.title);
+        System.out.println("Author: " + book3.author);
+        System.out.println("Genre: " + book3.genre);
+        System.out.println("Pages: " + book3.pages);
+        System.out.println("Price: Rs. " + book3.price);
+
+        System.out.println("--------------------------------------------------------------");
+
+        //book4
+        bookDetails book4 = new bookDetails();
+        book4.title = "The Alchemist";
+        book4.author = "Paulo Coelho";
+        book4.genre = "Adventure";
+        book4.pages = 208;
+        book4.price = 399;
+
+        // Print book4 details
+        System.out.println("Book Title: " + book4.title);
+        System.out.println("Author: " + book4.author);
+        System.out.println("Genre: " + book4.genre);
+        System.out.println("Pages: " + book4.pages);
+        System.out.println("Price: Rs. " + book4.price);
+
+        System.out.println("--------------------------------------------------------------");
+
+        //book5
+        bookDetails book5 = new bookDetails();
+        book5.title = "Educated";
+        book5.author = "Tara Westover";
+        book5.genre = "Memoir";
+        book5.pages = 334;
+        book5.price = 450;
+
+        // Print book5 details
+        System.out.println("Book Title: " + book5.title);
+        System.out.println("Author: " + book5.author);
+        System.out.println("Genre: " + book5.genre);
+        System.out.println("Pages: " + book5.pages);
+        System.out.println("Price: Rs. " + book5.price);
+
+        System.out.println("--------------------------------------------------------------");
+
+        //book6
+        bookDetails book6 = new bookDetails();
+        book6.title = "Dune";
+        book6.author = "Frank Herbert";
+        book6.genre = "Science Fiction";
+        book6.pages = 688;
+        book6.price = 799;
+
+        // Print book6 details
+        System.out.println("Book Title: " + book6.title);
+        System.out.println("Author: " + book6.author);
+        System.out.println("Genre: " + book6.genre);
+        System.out.println("Pages: " + book6.pages);
+        System.out.println("Price: Rs. " + book6.price);
+
+        System.out.println("--------------------------------------------------------------");
+
+        //book7
+        bookDetails book7 = new bookDetails();
+        book7.title = "Becoming";
+        book7.author = "Michelle Obama";
+        book7.genre = "Autobiography";
+        book7.pages = 448;
+        book7.price = 650;
+
+        // Print book7 details
+        System.out.println("Book Title: " + book7.title);
+        System.out.println("Author: " + book7.author);
+        System.out.println("Genre: " + book7.genre);
+        System.out.println("Pages: " + book7.pages);
+        System.out.println("Price: Rs. " + book7.price);
+
+        System.out.println("--------------------------------------------------------------");
+
+        //book8
+        bookDetails book8 = new bookDetails();
+        book8.title = "The Silent Patient";
+        book8.author = "Alex Michaelides";
+        book8.genre = "Psychological Thriller";
+        book8.pages = 336;
+        book8.price = 550;
+
+        // Print book8 details
+        System.out.println("Book Title: " + book8.title);
+        System.out.println("Author: " + book8.author);
+        System.out.println("Genre: " + book8.genre);
+        System.out.println("Pages: " + book8.pages);
+        System.out.println("Price: Rs. " + book8.price);
+
+        System.out.println("--------------------------------------------------------------");
+
+        //book9
+        bookDetails book9 = new bookDetails();
+        book9.title = "The Power of Now";
+        book9.author = "Eckhart Tolle";
+        book9.genre = "Spirituality";
+        book9.pages = 236;
+        book9.price = 399;
+
+        // Print book9 details
+        System.out.println("Book Title: " + book9.title);
+        System.out.println("Author: " + book9.author);
+        System.out.println("Genre: " + book9.genre);
+        System.out.println("Pages: " + book9.pages);
+        System.out.println("Price: Rs. " + book9.price);
+
+        System.out.println("--------------------------------------------------------------");
+    }
+}
