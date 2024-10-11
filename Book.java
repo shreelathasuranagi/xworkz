4b2e14e1a785bf77da894a7177d519dfa8a7fdda
shreelathasuranagi <shreelathasuranagi@gmail.com>
2024-10-11 21:32:36 +0530

    Classes and Objects

---
+++ Book.java
@@ -0,0 +1,30 @@
+public class Book {
+    
+    public static void main(String[] args){
+
+        //states
+        String title;
+        String author;
+        String genre;
+        int pages;
+        double price;
+    }
+
+
+    //behavior
+    public static void read(){
+
+    }
+
+    public static void displayInfo(){
+
+    }
+
+    public static void recommend() {
+
+    }
+
+    public static void getSummary() {
+
+    }
+}
